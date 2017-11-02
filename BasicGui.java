f4846e512ed72a1ac648eeddbfe6af869a3e1a44
Mark Chimes <markjchimes@gmail.com>
2017-11-02 20:27:43 +0200

    Accessibility via labels.

--- BasicGui.java
+++ BasicGui.java
@@ -14,20 +14,52 @@ public class BasicGui extends JFrame {
     private final JTextField textField;
     private final List<ActionListener> listeners = new ArrayList<>();
     private CustomTextKeyListener keyListener;
+    private final JLabel label;
+    private final JLabel label2;
+    private int listIndex = 0;
 
 
-        public BasicGui() {
-        setupBasicLayout();
-        textArea = new JTextArea("Welcome to Enceladus!");
-        textArea.setToolTipText("Display");
-        JScrollPane scroll = new JScrollPane(textArea);
-        add(scroll);
-        commands = new ArrayList<>();
-        textField = new JTextField();
-        textField.setToolTipText("Command");
-        setupTextArea();
-        createCommandableTextField();
-    }
+
+    public BasicGui() {
+//        setupBasicLayout();
+//        textArea = new JTextArea("Welcome to Enceladus!");
+//        textArea.setToolTipText("Display");
+//        JScrollPane scroll = new JScrollPane(textArea);
+//        add(scroll);
+//        commands = new ArrayList<>();
+//        textField = new JTextField();
+//        textField.setToolTipText("Command");
+//        setupTextArea();
+//        createCommandableTextField();
+
+
+            /****/
+            textArea = new JTextArea();
+            textField = new JTextField();
+
+            commands = new ArrayList<>();
+            setupBasicLayout();
+            label= new JLabel();
+            label2= new JLabel();
+
+            label.setText("MyText");
+            label2.setText("MyText");
+
+            add(label);
+            add(label2);
+
+            label.grabFocus();
+            this.addKeyListener(new LabelKeyListener());
+            this.getRootPane().addKeyListener(new LabelKeyListener());
+            label.addKeyListener(new LabelKeyListener());
+            label2.addKeyListener(new LabelKeyListener());
+
+            label.grabFocus();
+
+
+            /****/
+
+        }
 
     public final void addListener(ActionListener actionListener) {
         listeners.add(actionListener);
@@ -107,6 +139,67 @@ public class BasicGui extends JFrame {
                     for (ActionListener listener : listeners) {
                         listener.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, command));
                     }
+            }
+        }
+
+        public void resetListIndex() {
+            listIndex = 0;
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // DO NOTHING
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // DO NOTHING
+        }
+    }
+
+    private class LabelKeyListener implements KeyListener {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_UP:
+
+                    if (listIndex > 0) {
+                        listIndex--;
+                    } else {
+                        listIndex = commands.size() - 1;
+                    }
+
+                    readOnScreen();
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (listIndex < commands.size() - 1) {
+                        listIndex++;
+                    } else {
+                        listIndex = 0;
+                    }
+s
+                    readOnScreen();
+                    break;
+                case KeyEvent.VK_ENTER:
+                    String command = textField.getText();
+                    addToDisplayArea(command);
+                    textArea.grabFocus();
+                    int len = textArea.getDocument().getLength();
+                    textArea.setCaretPosition(len);
+                    for (ActionListener listener : listeners) {
+                        listener.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, command));
+                    }
+            }
+        }
+
+        private void readOnScreen() {
+            label.setText(commands.get(listIndex));
+            label2.setText(commands.get(listIndex));
+            if (label.hasFocus()) {
+                label2.grabFocus();
+            } else {
+                label.grabFocus();
             }
         }
 
