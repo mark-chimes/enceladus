cf2a4f261f86c72048e843066849f0e4c1f80498
Mark Chimes <markjchimes@gmail.com>
2017-11-05 15:13:32 +0200

    Navigate menu and show helpful starting hints.

--- GameLoop.java
+++ GameLoop.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class GameLoop {
     private final BasicGui gui;
     private TextIterator textIterator;
-    private DownListener downListener;
+    private SkipTextListener skipListener;
 
     public GameLoop(BasicGui gui) {
         this.gui = gui;
@@ -20,18 +20,33 @@ public class GameLoop {
         EventQueue.invokeLater(() -> {
             gui.setVisible(true);
         });
-        downListener = new DownListener();
-        addGuiTextList(welcomeMessage());
+        skipListener = new SkipTextListener();
+        ArrayList<String> texts = welcomeMessage();
+        textIterator = new TextIterator(gui, texts);
+        gui.setText(texts.get(0));
+        gui.addListener(textIterator);
+        gui.addListener(skipListener);
     }
 
     public ArrayList<String> welcomeMessage() {
         ArrayList<String> welcomeMessage = new ArrayList<>();
-        String previous = KeyEvent.getKeyText(KeyConstants.PREVIOUS_TEXT);
-        String next = KeyEvent.getKeyText(KeyConstants.NEXT_TEXT);
-        String skip = KeyEvent.getKeyText(KeyConstants.SKIP_TEXT);
+        String previousText = KeyEvent.getKeyText(KeyConstants.PREVIOUS_TEXT);
+        String nextText = KeyEvent.getKeyText(KeyConstants.NEXT_TEXT);
+        String skipText = KeyEvent.getKeyText(KeyConstants.SKIP_TEXT);
+
+        String previousItem = KeyEvent.getKeyText(KeyConstants.PREVIOUS_ITEM);
+        String nextItem = KeyEvent.getKeyText(KeyConstants.NEXT_ITEM);
+        String selectItem = KeyEvent.getKeyText(KeyConstants.CONFIRM);
+
+        String previousMenu = KeyEvent.getKeyText(KeyConstants.PREVIOUS_MENU);
         welcomeMessage.add(MessageFormat.format("Welcome to Enceladus! Press {0} and {1} to read, " +
-                "and {2} to skip.", previous, next, skip));
+                "and {2} to skip.", previousText, nextText, skipText));
+        welcomeMessage.add(MessageFormat.format("Whilst in a menu, press {0} and {1} to change options, " +
+                "and {2} to select.", previousItem, nextItem, selectItem));
+        welcomeMessage.add(MessageFormat.format("You can also press {0} to go to the previous menu.",
+                previousMenu));
         welcomeMessage.add("If this is your first time playing, please read the instructions.");
+        welcomeMessage.add(MessageFormat.format("End of text. Press {0} to continue.", selectItem));
         return welcomeMessage;
     }
 
@@ -46,7 +61,7 @@ public class GameLoop {
     }
 
     public void changeGuiTextList(java.util.List<String> texts) {
-        gui.removeKeyListener(textIterator);
+
         addGuiTextList(texts);
     }
 
@@ -56,11 +71,17 @@ public class GameLoop {
         gui.addListener(textIterator);
     }
 
-    private class DownListener extends KeyAdapter {
+    private class SkipTextListener extends KeyAdapter {
         @Override
         public void keyReleased(KeyEvent e) {
             switch (e.getKeyCode()) {
-                case KeyEvent.VK_DOWN:
+                case KeyConstants.SKIP_TEXT:
+                    System.out.println("Got skip code");
+                    gui.removeKeyListener(textIterator);
+                    ArrayList<String> items = initialCommands();
+                    ItemIterator optionsIterator = new ItemIterator(gui, items);
+                    gui.addListener(optionsIterator);
+                    gui.setText(items.get(0));
                     break;
             }
         }
